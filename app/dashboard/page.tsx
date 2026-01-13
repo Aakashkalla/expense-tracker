ad3b4c514f5ff81f8e69a56006e0dba63176e35d
Aakashkalla <aakashpradeepkalla@gmail.com>
2026-01-13 19:40:55 +0530

    updated dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,38 @@
 import prisma from "@/lib/prisma";
-import DashboardClient from "@/app/dashboard/DashboardClient";
+import { redirect } from "next/navigation";
+import DashboardClient from "./DashboardClient";
 
 export default async function DashboardPage() {
-  const expenses = await prisma.expense.findMany({
-    where: {
-      userId: "temp-user", // auth later
-      month: "2026-01",
-    },
-    orderBy: { createdAt: "desc" },
-  });
+  const userId = "temp-user";
+  const month = "2026-01";
 
   const monthlyConfig = await prisma.monthlyConfig.findUnique({
-  where: {
-    userId_month: {
-      userId: "temp-user",
-      month: "2026-01",
+    where: {
+      userId_month: {
+        userId,
+        month,
+      },
     },
-  },
-});
+  });
 
-  return <DashboardClient expenses={expenses} monthlyConfig={monthlyConfig} />;
+  if (!monthlyConfig) {
+    redirect("/setup");
+  }
+
+  const expenses = await prisma.expense.findMany({
+    where: {
+      userId,
+      month,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <DashboardClient
+      expenses={expenses}
+      monthlyConfig={monthlyConfig}
+    />
+  );
 }
